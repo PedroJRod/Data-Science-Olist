80243752a3772fbb2f20280bb6daf3af387f0fe7
Pedro José <pj-rod@hotmail.com>
2024-11-03 06:55:48 -0400

    a

--- salvar_db.py
+++ salvar_db.py
@@ -5,11 +5,6 @@ from sqlalchemy import create_engine
 # Configurações de conexão ao banco de dados PostgreSQL
 postgres_url = "postgresql+psycopg2://aluno:PreditivaAlunoGVD@preditiva-postgres-1.cmaonsvyciyn.us-east-1.rds.amazonaws.com/alunos"
 
-#"postgresql+psycopg2://scott:tiger@localhost:5432/mydatabase"
-
-# Caminho do banco de dados SQLite de destino
-destino_db = r"C:\Users\Acer\Desktop\TESTE\meu_banco.db"
-
 # Conectar ao banco de dados PostgreSQL (origem)
 engine_origem = create_engine(postgres_url)
 
@@ -18,6 +13,9 @@ engine_destino = create_engine(f"sqlite:///{destino_db}")
 
 # Lista de tabelas para exportar
 tabelas = ["db_churn.dim_clientes", "db_churn.fato_churn"]
+
+# Caminho do banco de dados SQLite de destino
+destino_db = r"C:\Users\Acer\Desktop\TESTE\meu_banco.db"
 
 # Função para copiar a tabela para o banco de dados SQLite de destino
 def copiar_tabela(table_name):
